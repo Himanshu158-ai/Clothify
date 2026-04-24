d38a14f939bf55fb5f09f72046864e9759590eef
Himanshu Singh <hs5317610@gmail.com>
2026-04-24 22:58:50 +0530

    add profile route

---
+++ server/src/middleware/auth.middleware.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+import { config } from "../config/config.js";
+
+const verifyToken = (req,res,next) => {
+    const token = req.cookies.token;
+    if(!token){
+        return res.status(401).json({message:"Unauthorized"});
+    }
+    try{
+        const decoded = jwt.verify(token,config.jwt_secret);
+        req.userId = decoded.id;
+        next();
+    }
+    catch(err){
+        return res.status(401).json({message:"Unauthorized"});
+    }
+}
+
+export default verifyToken;
--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import {validateRegister,validateLogin} from '../validator/auth.validation.js';
-import { registerUser,loginUser } from '../controllers/auth.controller.js';
+import { registerUser,loginUser , getProfile} from '../controllers/auth.controller.js';
+import verifyToken from '../middleware/auth.middleware.js';
 const router = express.Router();
 
 
 router.post('/register',validateRegister,registerUser);
-router.post('/login',validateLogin,loginUser)
-
+router.post('/login',validateLogin,loginUser);
+router.get('/profile',verifyToken,getProfile);
 
 
 export default router;
