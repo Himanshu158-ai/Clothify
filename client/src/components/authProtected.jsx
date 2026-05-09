a6ced69f901673b736ebdc3a4644061714f4dd14
Himanshu Singh <hs5317610@gmail.com>
2026-05-09 21:33:43 +0530

    feat: create ProtectedRoute component to handle authentication state and redirection

--- client/src/components/authProtected.jsx
+++ client/src/components/authProtected.jsx
@@ -5,7 +5,7 @@ import { Navigate } from 'react-router-dom'
 export default function ProtectedRoute({ children }) {
   const { user ,loading} =  useSelector((state) => state.auth);
   
-  if (loading) return <p>Loading...</p>
+  if (loading) return <p className='text-red-800 text-2xl flex justify-center items-center h-screen'>Loading...</p>
   if (!user) return <Navigate to="/login" />
   
   return children
